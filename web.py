9af10842644857f734492017f027e629e43c1cb6
FrancineTiba <francine_tiba@hotmail.com>
2025-04-29 21:45:59 -0300

    Add files via upload

--- web.py
+++ web.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import datetime
+
+# Define a data mínima permitida (1º de janeiro de 1970)
+data_minima = datetime.date(1970, 1, 1)
+
+# Define a data máxima permitida (você pode ajustar conforme necessário,
+# por exemplo, a data atual ou um ano futuro razoável)
+data_maxima = datetime.date.today() # Usando a data atual como exemplo
 
 #Terminal > New Terminal > pip install streamlit > pressiona enter que irá instalar o Streamlit
 
@@ -12,6 +20,7 @@ st.write("Hello Word! I am back!")
 
 nome = st.text_input('Qual seu nome?')
 idade = st.number_input('Idade?', step=1, value=0, format="%d")
+dtNasc = st.date_input('Data de nascimento?', min_value=data_minima, max_value=data_maxima)
 
 st.write(f'Olá {nome} de {idade} anos!')
 
